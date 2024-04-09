3296131b2d34d26a8ed04c42f9f2a46df1d4ab4d
River <coco0958231520@gmail.com>
2024-04-10 01:14:10 +0800

    complete main ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,52 @@
+'use client';
+import { useRef } from 'react';
+import { HomeWrapper } from './style';
+
 export default function Home() {
-  return <div></div>;
+  const airportInput = '桃園國際機場 第一航廈';
+  const airlineIdRef = useRef<HTMLDivElement>(null);
+  const nameRef = useRef<HTMLDivElement>(null);
+  const phoneNumberRef = useRef<HTMLDivElement>(null);
+  const idRef = useRef<HTMLDivElement>(null);
+  // const formRefs = { airlineIdRef, nameRef, phoneNumberRef, idRef };
+
+  // const submit = () => {
+
+  // };
+  const formError = {
+    airlineId: true,
+    name: true,
+    phoneNumber: true,
+    id: true,
+  };
+
+  return (
+    <HomeWrapper>
+      <div className="title">送機行程</div>
+      <div className="subTitle">送機計畫</div>
+      <div className="form_item">下車機場</div>
+      <div className="input" id="airport">
+        {airportInput}
+      </div>
+      <div className="form_item">航班編號</div>
+      <div className={`input ${formError.airlineId && 'error'}`} contentEditable ref={airlineIdRef} id="airlineId" />
+      <div className="subTitle">旅客資訊</div>
+      <div className="form_item">姓名</div>
+      <div className={`input ${formError.name && 'error'}`} contentEditable ref={nameRef} id="name" />
+      <div className="form_item">電話</div>
+      <div
+        className={`input ${formError.phoneNumber && 'error'}`}
+        contentEditable
+        ref={phoneNumberRef}
+        id="phoneNumber"
+      />
+      <div className="form_item" ref={idRef}>
+        身分證字號/護照編號
+      </div>
+      <div contentEditable className="input" id="id" />
+      <div className="form_item">乘車備註</div>
+      <div className={`input ${formError.id && 'error'}`} contentEditable id="remark" />
+      <div className="button">下一步</div>
+    </HomeWrapper>
+  );
 }
