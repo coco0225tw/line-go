ed5b3d33f81d0dd4d22a147fe9c4248c845e9faf
River <coco0958231520@gmail.com>
2024-04-11 22:59:02 +0800

    fix regex

--- src/app/components/form/index.tsx
+++ src/app/components/form/index.tsx
@@ -9,16 +9,16 @@ export default function Form() {
   const nameRef = useRef<HTMLDivElement>(null);
   const phoneNumberRef = useRef<HTMLDivElement>(null);
   const idRef = useRef<HTMLDivElement>(null);
-  const [formError, setFormError] = useState({
-    airlineId: false,
-    name: false,
-    phoneNumber: false,
-    id: false,
+  const [formValidate, setFormValidate] = useState({
+    airlineId: true,
+    name: true,
+    phoneNumber: true,
+    id: true,
   });
-  const airlineIdRegex = new RegExp('[^A-Za-z0-9]+');
-  const nameRegex = new RegExp('[^A-Za-z ]+');
-  const phoneNumberRegex = new RegExp('[0-9]+');
-  const idRegex = new RegExp('[^A-Z0-9]+');
+  const airlineIdRegex = new RegExp('^[A-Za-z\\d]+$');
+  const nameRegex = new RegExp('^[A-Za-z\\s]+[A-Za-z]+[A-Za-z\\s]$');
+  const phoneNumberRegex = new RegExp('^[\\d]+$');
+  const idRegex = new RegExp('^[A-Z\\d]+$');
 
   const submit = () => {
     //todo callApi
@@ -31,12 +31,13 @@ export default function Form() {
     }
   };
   const checkForm = () => {
-    const checkAirlineId = airlineIdRegex.test(modifyTypeToString(airlineIdRef));
-    const checkName = nameRegex.test(modifyTypeToString(nameRef));
-    const checkPhoneNumber = phoneNumberRegex.test(modifyTypeToString(phoneNumberRef));
-    const checkId = idRegex.test(modifyTypeToString(idRef));
-    setFormError({
-      ...formError,
+    const checkAirlineId = checkValue(airlineIdRef, airlineIdRegex);
+    const checkName = checkValue(nameRef, nameRegex);
+    const checkPhoneNumber = checkValue(phoneNumberRef, phoneNumberRegex);
+    const checkId = checkValue(idRef, idRegex);
+
+    setFormValidate({
+      ...formValidate,
       airlineId: checkAirlineId,
       name: checkName,
       phoneNumber: checkPhoneNumber,
@@ -47,9 +48,15 @@ export default function Form() {
     }
     return true;
   };
-  const modifyTypeToString = (inputRef: RefObject<HTMLDivElement>) => {
+
+  const checkValue = (inputRef: RefObject<HTMLDivElement>, regex: RegExp) => {
     const inputValue = inputRef?.current?.textContent;
-    return inputValue ? inputValue : '';
+
+    function isDefined<T>(val: T | undefined | null): val is T {
+      return val !== undefined && val !== null;
+    }
+    if (isDefined(inputValue)) return regex.test(inputValue);
+    return false;
   };
 
   return (
@@ -61,23 +68,26 @@ export default function Form() {
         {airportInput}
       </div>
       <div className="form_item">航班編號</div>
-      <div className={`input ${formError.airlineId && 'error'}`} contentEditable ref={airlineIdRef} id="airlineId" />
+      <div
+        className={`input ${!formValidate.airlineId && 'error'}`}
+        contentEditable
+        ref={airlineIdRef}
+        id="airlineId"
+      />
       <div className="subTitle">旅客資訊</div>
       <div className="form_item">姓名</div>
-      <div className={`input ${formError.name && 'error'}`} contentEditable ref={nameRef} id="name" />
+      <div className={`input ${!formValidate.name && 'error'}`} contentEditable ref={nameRef} id="name" />
       <div className="form_item">電話</div>
       <div
-        className={`input ${formError.phoneNumber && 'error'}`}
+        className={`input ${!formValidate.phoneNumber && 'error'}`}
         contentEditable
         ref={phoneNumberRef}
         id="phoneNumber"
       />
-      <div className="form_item" ref={idRef}>
-        身分證字號/護照編號
-      </div>
-      <div contentEditable className="input" id="id" />
+      <div className="form_item">身分證字號/護照編號</div>
+      <div contentEditable ref={idRef} className={`input ${!formValidate.id && 'error'}`} id="id" />
       <div className="form_item">乘車備註</div>
-      <div className={`input ${formError.id && 'error'}`} contentEditable id="remark" />
+      <div contentEditable id="remark" />
       <Button theme={ButtonTheme1.Dark} message="下一步" submitFun={action} />
     </FormWrapper>
   );
