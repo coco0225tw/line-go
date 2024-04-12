8007247e95d29ea7de0723181d7d52c859abc32f
River <coco0958231520@gmail.com>
2024-04-13 04:41:43 +0800

    rwd ,scroll bar

--- src/app/style.ts
+++ src/app/style.ts
@@ -3,4 +3,8 @@ export const HomeWrapper = styled.div`
   width: 100%;
   height: 100%;
   color: #4b5c6b;
+  @media screen and (max-width: 768px) {
+    width: 768px;
+  }
+  position: relative;
 `;
